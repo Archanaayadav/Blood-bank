59fca00cdf37532654d7785cfbafa9a08698e62e
Archana Yadav <ahana.archana23@gmail.com>
2021-07-16 23:53:36 +0530

    updated

---
+++ main.js
@@ -0,0 +1,9 @@
+
+// Sticky menu background (Regular Javascript)
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
